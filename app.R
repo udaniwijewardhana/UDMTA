4359f69730a14520924b70b9cb2ffb58ddc48aaf
Udani Wijewardhana <55830031+uwijewardhana@users.noreply.github.com>
2021-06-25 22:22:26 +1000

    Add files via upload

--- app.R
+++ app.R
@@ -24,7 +24,7 @@ library(INLA)
 
 ui <- fluidPage(
   
-titlePanel(strong("UDM - Shiny App for Annual Species Temporal Abundance Models", titleWidth = 350)),
+titlePanel(strong("UDMTA - A shiny App for Annual Species Temporal Abundance Models", titleWidth = 350)),
   
 # Loading the data file
 div(style="display: inline-block;vertical-align:top; width: 300px;", fileInput("file", "Choose data CSV File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv"))),
@@ -43,12 +43,11 @@ tabPanel("Data",
 tabPanel("Species Distribution Model",
          sidebarLayout(
          sidebarPanel(div(style='height:950px; overflow: scroll',
-         selectInput("type", "Model type (Multi-species or Single species", choices=c("Multi Species", "Single Species"), selected = "Multi Species"),
          selectInput("distribution", "Distribution:", choices=c("Poisson", "Negative Binomial",
                                                       "Zeroinflated Poisson", "Zeroinflated Negative Binomial",
                                                       "Poisson Hurdle", "Negative Binomial Hurdle"), selected = "Poisson"),
          selectInput("tempeffect", "temporal random effect model:", choices=c("'ar1'", "'iid'", "'rw1'", "'rw2'"), selected = "'ar1'"),
-         selectInput("controlgroup", "control group model (multispecies):", choices=c("'iid'", "'exchangeable'"), selected = "'exchangeable'"),
+         selectInput("factor", "Include factor variables in the model:", choices=c("No", "Yes"), selected = "No"),
          h5('Generate Interaction Variables Here (if applicable)'),
          uiOutput("independent"),
          uiOutput("makeInteract1"), uiOutput("makeInteract2"),
@@ -136,12 +135,20 @@ observe({
     interacts[[b]] <- a
 })})
 
-all <- reactive({
+# Create dataframe for regression only with numeric predictor variables
+num <- reactive({
   inFile <- input$file
   if (is.null(inFile)){return(NULL)}
   x <- as.data.frame(read.csv(inFile$datapath, fileEncoding="UTF-8-BOM"))
-  
+
+  if(input$distribution == "Poisson Hurdle" | input$distribution == "Negative Binomial Hurdle"){
+    x$Count[x$Count == 0] <- NA
+  } else {
+    x$Count = x$Count
+  }
+
   y = dplyr::select_if(x, is.numeric)
+  
   if(ncol(y)>2){
     p = subset(y, select = -c(Count))
     p <- unique(p)
@@ -154,25 +161,73 @@ all <- reactive({
   }else if(input$prednorm == "stand") {p[,i] <- round(scale(p[,i]), digits = 4)
   }else {p[,i] <- p[,i]}}}
   
-  xx = cbind(Year = unique(x$Year), p)
+  d1 = cbind(Year = unique(x$Year), p, effect = unique(x$Year))
+  d2 <- aggregate(Count ~ Species + Year, x, FUN = sum)
+  d2$ID <- paste(d2$Species, d2$Year, sep = "-", collapse = NULL)
+  d3 <- d1[rep(seq_len(nrow(d1)), length(unique(x$Species))), ]
+  d3$Species <- rep(unique(x$Species), each = length(unique(x$Year)))
+  d3$ID <- paste(d3$Species, d3$Year, sep = "-", collapse = NULL)
+  d4 <- join(d3, data, by = "ID", type = "left", match = "all")
+  d4 <- d4[order(d4$Species, d4$Year),]
+  d3 <- d3[ , !(names(d3) %in% c("ID"))]
+  Count = d4$Count
+  Final <- cbind(d3, Count)
   
-  if(is.null(p)){
-    Final = x
-  }else {
-    z = dplyr::select_if(x, is.factor)
-    q <- x[ , (names(x) %in% c("Species", "Year", "Count"))]
-    n = nrow(x)/length(unique(x$Year))
-    p <- xx[rep(seq_len(nrow(xx)), n), ]
-    
-    Final = cbind(q, subset(p, select = -c(Year)), subset(z, select = -c(Species)))
-  }
   return(Final)
+})
+
+# Create dataframe for regression with categorical predictor variables
+
+fac <- reactive({
+    inFile <- input$file
+    if (is.null(inFile)){return(NULL)}
+    x <- as.data.frame(read.csv(inFile$datapath, fileEncoding="UTF-8-BOM"))
+    
+    fac = data.frame(x %>% select_if(~ !((is.integer(.x)) | (is.numeric(.x)))))
+    for(i in 1:ncol(fac)){fac[,i] = as.factor(fac[,i])}  
+    y = dplyr::select_if(x, is.numeric)
+    x <- cbind(y,fac)
+    
+    if(input$distribution == "Poisson Hurdle" | input$distribution == "Negative Binomial Hurdle"){
+      x$Count[x$Count == 0] <- NA
+    } else {
+      x$Count = x$Count
+    }
+
+    if(ncol(y)>2){
+      p = subset(y, select = -c(Count))
+      p <- unique(p)
+      p = subset(p, select = -c(Year))
+    }else {p = NULL}
+    
+    if(!is.null(p)){
+    for(i in 1:ncol(p)){
+    if(input$prednorm == "rnorm"){p[,i] <- round(rnorm(p[,i]), digits = 4)
+    }else if(input$prednorm == "stand") {p[,i] <- round(scale(p[,i]), digits = 4)
+    }else {p[,i] <- p[,i]}}}
+    
+    xx = cbind(Year = unique(x$Year), p, effect = unique(x$Year))
+    
+    if(is.null(p)){
+      Final = x
+    }else {
+      z = dplyr::select_if(x, is.factor)
+      Count <- x[ , (names(x) %in% c("Count"))]
+      n = nrow(x)/length(unique(x$Year))
+      p <- xx[rep(seq_len(nrow(xx)), n), ]
+      
+      Final = cbind(p, Count, z)
+    }
+    return(Final)
 })
 
 # Checkbox list of all numeric variables to use 
 independent <- reactive({
     if(!is.null(input$file)){
-    df = all()[ , !(names(all()) %in% c("Count"))]
+    inFile <- input$file
+
+    x <- as.data.frame(read.csv(inFile$datapath, fileEncoding="UTF-8-BOM"))
+    df = x[ , !(names(x) %in% c("Count", "Species"))]
     return(names(df))
     }
 })
@@ -182,109 +237,71 @@ output$independent <- renderUI({checkboxGroupInput("independent", "Independent (
 # Variables to Add to the List of Combinations  
 makeInteract <- reactive({
     if(!is.null(input$file)){
-    df = all()[ , !(names(all()) %in% c("Count"))]
-    return(names(df))}
+    inFile <- input$file
+
+    x <- as.data.frame(read.csv(inFile$datapath, fileEncoding="UTF-8-BOM"))
+    df = x[ , !(names(x) %in% c("Count", "Species"))]    
+    return(names(df))
+    }
 })
   
 output$makeInteract1 <- renderUI({selectInput("makeInteract1", "Variable1 For Interaction:", makeInteract())})
 output$makeInteract2 <- renderUI({selectInput("makeInteract2", "Variable2 For Interaction:", makeInteract())})
 
+# distribution
+distribution <- reactive({
+  if(input$distribution == "Poisson"){distribution = "poisson"
+  } else if(input$distribution == "Negative Binomial"){distribution = "nbinomial"
+  } else if(input$distribution == "Zeroinflated Poisson") {distribution = "zeroinflatedpoisson1"
+  } else if(input$distribution == "Zeroinflated Negative Binomial") {distribution = "zeroinflatednbinomial1"
+  } else if(input$distribution == "Poisson Hurdle") {distribution = "zeroinflatedpoisson0"
+  } else {distribution = "zeroinflatednbinomial0"}
+  return(distribution)
+})
+
+# formula
+formula <- reactive({
+  if(!is.null(input$added)){
+  formula = paste("Count ~ 1 +", paste(input$independent, collapse = "+"), 
+            paste("+", paste(input$added, collapse = "+")), 
+            paste("+", "f(effect, model = ", input$tempeffect, ")"))
+  }else {
+  formula = paste("Count ~ 1 + ", paste(input$independent, collapse = "+"), 
+            paste("+", "f(effect, model = ", input$tempeffect, ")"))
+  }
+  return(formula)
+})
+
 # Fit SDM using R-INLA
   
 fitsummary <- reactive({
     
-    if(input$type == "Multi Species"){
-      
-    if(!is.null(input$independent)){
-      df <- unique(all()[ , (names(all()) %in% c(as.vector(paste(input$independent, sep = ",")), "Species", "Count", "Year"))])
-    }
-    
-    # "Count" for the specific distribution
-    count = df$Count
-      
-    if(input$distribution == "Poisson Hurdle" | input$distribution == "Negative Binomial Hurdle"){
-    count[count == 0] <- NA
-    } else {count = count}
-      
-    df$Count <- count
-    df$effect <- df$Year
-    df$group <- rep(c(1:length(unique(df$Species))), each = nrow(df)/length(unique(df$Species)))
-      
-    # distribution
-    if(input$distribution == "Poisson"){distribution = "poisson"
-    } else if(input$distribution == "Negative Binomial"){distribution = "nbinomial"
-    } else if(input$distribution == "Zeroinflated Poisson") {distribution = "zeroinflatedpoisson1"
-    } else if(input$distribution == "Zeroinflated Negative Binomial") {distribution = "zeroinflatednbinomial1"
-    } else if(input$distribution == "Poisson Hurdle") {distribution = "zeroinflatedpoisson0"
-    } else {distribution = "zeroinflatednbinomial0"}
-    
-    if(!is.null(input$added)){
-    formula = as.formula(paste("Count ~ 1 +", paste(input$independent, collapse = "+"), 
-              paste("+", paste(input$added, collapse = "+")), 
-              paste("+", "f(effect, model = ", input$tempeffect, ", group = group",
-              ", control.group = list(model = ", input$controlgroup,"))")))
-    
-    }else{
-    formula = as.formula(paste("Count ~ 1 +", paste(input$independent, collapse = "+"), 
-              paste("+", "f(effect, model = ", input$tempeffect, ", group = group",
-              ", control.group = list(model = ", input$controlgroup,"))")))
-    }
-    
-    model <- INLA::inla(formula, data = df, family = distribution,
-                        control.family = list(link = "log"),
-                        control.compute = list(dic = TRUE, cpo = TRUE, po = TRUE), verbose = TRUE)
-    return(model$summary.fixed[,c(1:3,5)])
-    
-    }else {
-      
-    if(!is.null(input$independent)){
-      df <- unique(all()[ , (names(all()) %in% c(as.vector(paste(input$independent, sep = ",")), "Species", "Count", "Year"))])
-    }      
-    
-    # "Count" for the specific distribution
-    count = df$Count
-      
-    if(input$distribution == "Poisson Hurdle" | input$distribution == "Negative Binomial Hurdle"){
-    count[count == 0] <- NA
-    } else {count = count}
-      
-    df$Count <- count
-    df$effect <- df$Year
-      
-    # distribution
-    if(input$distribution == "Poisson"){distribution = "poisson"
-    } else if(input$distribution == "Negative Binomial"){distribution = "nbinomial"
-    } else if(input$distribution == "Zeroinflated Poisson") {distribution = "zeroinflatedpoisson1"
-    } else if(input$distribution == "Zeroinflated Negative Binomial") {distribution = "zeroinflatednbinomial1"
-    } else if(input$distribution == "Poisson Hurdle") {distribution = "zeroinflatedpoisson0"
-    } else {distribution = "zeroinflatednbinomial0"}
-      
-    lst <- split(df, df$Species) 
-      
-    if(!is.null(input$added)){
-    
-    formula = as.formula(paste("Count ~ 1 +", paste(input$independent, collapse = "+"), 
-              paste("+", paste(input$added, collapse = "+")), 
-              paste("+", "f(effect, model = ", input$tempeffect, ")")))
-    }else {
-    formula = as.formula(paste("Count ~ 1 + ", paste(input$independent, collapse = "+"), 
-              paste("+", "f(effect, model = ", input$tempeffect, ")")))
-    }
-    
+    df1 <- as.data.frame(fac())
+    df2 <- as.data.frame(num())
+ 
     model <- list()
     results <- list()
-
-    for (i in 1:length(unique(df$Species))){
-      
-    model[[i]] <- INLA::inla(formula, data = lst[[i]], family = distribution,
-                  control.family = list(link = "log"),
-                  control.compute = list(dic = TRUE,cpo = TRUE, po = TRUE), verbose = TRUE)
+    lst1 <- split(df1, df1$Species) 
+    lst2 <- split(df2, df2$Species) 
     
-    results[[i]] <- model[[i]]$summary.fixed[,c(1:3,5)]
+    if(input$factor == "Yes"){
+      
+    model <- lapply(seq_along(1:length(unique(df1$Species))), function(x)
+                    inla(as.formula(formula()), data = lst1[[x]], family = distribution(),
+                    control.family = list(link = "log"),
+                    control.compute = list(dic = TRUE, cpo = TRUE, config = TRUE)))
+    results <- lapply(seq_along(1:length(unique(df1$Species))), function(x) model[[x]]$summary.fixed[,c(1:3,5)])
+    
+    }else {
+  
+    model <- lapply(seq_along(1:length(unique(df2$Species))), function(x)
+                    inla(as.formula(formula()), data = lst2[[x]], family = distribution(),
+                    control.family = list(link = "log"),
+                    control.compute = list(dic = TRUE, cpo = TRUE)))
+    results <- lapply(seq_along(1:length(unique(df2$Species))), function(x) model[[x]]$summary.fixed[,c(1:3,5)])
     
     }
     return(results)
-    }
 })
   
 # Summary output of SDM
